01faa4815ce38790c197714b518697813b7fe13d
Connor <connor.doherty97@gmail.com>
2022-10-24 15:48:51 -0400

    stocks and crypto separated

--- frontend/src/components/Watchlist.js
+++ frontend/src/components/Watchlist.js
@@ -11,13 +11,18 @@ function Watchlist(props){
         <div>
             
             <NotFound found = {props.tickerFound} text = {props.tickerInput}/>
-        
+            <h2>Stocks</h2>
             {props.tickers.map((el,index) =>{
-                
+                if(el.type==='stock'){
                 return <SavedTickers key = {props.tickers[index]._id} ticker = {el} state = {props.tickers} setState = {props.setState} index={index}/>
-                
+                }
             })}
-            
+            <h2>Crypto</h2>
+            {props.tickers.map((el,index) =>{
+                if(el.type==='crypto'){
+                return <SavedTickers key = {props.tickers[index]._id} ticker = {el} state = {props.tickers} setState = {props.setState} index={index}/>
+                }
+            })}
         </div>
     )
     }

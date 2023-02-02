61054dacf4d70ad98bd1191880981631d3c7027b
Connor Doherty <92645318+dohboy97@users.noreply.github.com>
2023-02-01 22:07:34 -0500

    moved update func to writes

--- frontend/src/api/databaseCalls/writes.js
+++ frontend/src/api/databaseCalls/writes.js
@@ -47,3 +47,19 @@ export async function addToWatchList({ input, watchList, selectedMarket }) {
   const data = await res.json();
   return data;
 }
+
+export async function updateStockPrice({ ticker }) {
+  const res = await fetch(`/watchlist/updateticker/${ticker._id}`, {
+    method: "PUT",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({
+      symbol: ticker.symbol,
+      price: ticker.price,
+      type: ticker.type,
+      index: ticker.index,
+    }),
+  });
+  const data = await res.json();
+
+  return data;
+}

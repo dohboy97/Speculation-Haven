b7f42e88b7b2b0e3c66e13d7c69f392f3a7d9ce8
Connor Doherty <92645318+dohboy97@users.noreply.github.com>
2023-02-04 15:55:09 -0500

    removing be console logs

--- backend/controllers/watchList.js
+++ backend/controllers/watchList.js
@@ -1,4 +1,4 @@
-const WatchList = require('../models/WatchList')
+const WatchList = require("../models/WatchList");
 const Alpaca = require("@alpacahq/alpaca-trade-api");
 
 //alpaca class and key info
@@ -8,79 +8,68 @@ const alpaca = new Alpaca({
   secretKey: process.env.API_SECRET,
   paper: true,
 });
-// async function test() {
-// let crypt = await alpaca.getLatestCryptoTrade('AVAXUSD',{exchange:'FTXU'})
-// console.log(crypt)
-// }
-// test()
-
 
 module.exports = {
-    getWatchList: async(req,res)=>{
-        try{
-            const watchList = await WatchList.find()
-            res.json({stonks:watchList})
-        }catch(err){
-            console.log(err)
-        }
-    },
-
-    addTicker: async(req,res)=>{
-        console.log(req.body)
-        
-        try{
-            let trade
-            if(req.body.type === 'stock'){
-            trade = await alpaca.getLatestTrade(req.params.id);
-            }else{
-            trade = await alpaca.getLatestCryptoTrade(`${req.params.id}USD`,{exchange:'FTXU'})
-            }
-            console.log(trade)
-            await WatchList.create({
-                symbol:req.params.id.toUpperCase(),
-                price:trade.Price,
-                type:req.body.type,
-                index:req.body.index
-            })
-            const watchList = await WatchList.find()
-            
-            res.json({stonks:watchList,
-            ticker:true})
-             console.log('ticker added')
-        }catch(err){
-            console.log('test',err)
-            res.json({ticker:false})
-        }
-    },
-    deleteTicker: async (req,res)=>{
-        console.log(req.params.id)
-        await WatchList.findByIdAndDelete(req.params.id)
-    },
-
-    updateTicker: async(req,res)=>{
-        console.log(req.params.id)
-       console.log(req.body.symbol)
-        
-        try{
-            let trade 
-            if(req.body.type==='stock'){
-            trade = await alpaca.getLatestTrade(req.body.symbol);
-            }else{
-                trade = await alpaca.getLatestCryptoTrade(`${req.body.symbol}USD`,{exchange:'FTXU'})
-            }
-            console.log(trade)
-            await WatchList.findByIdAndUpdate({_id: req.params.id},{
-                price: trade.Price             
-                
-            })
-            const updatedTicker = await WatchList.find({_id: req.params.id})
-            
-            res.json({updatedStonk:updatedTicker})
-             console.log('ticker updated')
-        }catch(err){
-            console.log(err)
-           
-        }
+  getWatchList: async (req, res) => {
+    try {
+      const watchList = await WatchList.find();
+      res.json({ stonks: watchList });
+    } catch (err) {
+      console.log(err);
     }
+  },
 
-}
+  addTicker: async (req, res) => {
+    try {
+      let trade;
+      if (req.body.type === "stock") {
+        trade = await alpaca.getLatestTrade(req.params.id);
+      } else {
+        trade = await alpaca.getLatestCryptoTrade(`${req.params.id}USD`, {
+          exchange: "FTXU",
+        });
+      }
+      console.log(trade);
+      await WatchList.create({
+        symbol: req.params.id.toUpperCase(),
+        price: trade.Price,
+        type: req.body.type,
+        index: req.body.index,
+      });
+      const watchList = await WatchList.find();
+
+      res.json({ stonks: watchList, ticker: true });
+    } catch (err) {
+      console.log("test", err);
+      res.json({ ticker: false });
+    }
+  },
+  deleteTicker: async (req, res) => {
+    await WatchList.findByIdAndDelete(req.params.id);
+  },
+
+  updateTicker: async (req, res) => {
+    try {
+      let trade;
+      if (req.body.type === "stock") {
+        trade = await alpaca.getLatestTrade(req.body.symbol);
+      } else {
+        trade = await alpaca.getLatestCryptoTrade(`${req.body.symbol}USD`, {
+          exchange: "FTXU",
+        });
+      }
+      console.log(trade);
+      await WatchList.findByIdAndUpdate(
+        { _id: req.params.id },
+        {
+          price: trade.Price,
+        }
+      );
+      const updatedTicker = await WatchList.find({ _id: req.params.id });
+
+      res.json({ updatedStonk: updatedTicker });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};

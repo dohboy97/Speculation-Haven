ef5d618d0ec8676992af4c502164c5f83c75a475
Connor Doherty <92645318+dohboy97@users.noreply.github.com>
2023-02-04 15:55:55 -0500

    removing more console logs

--- backend/controllers/search.js
+++ backend/controllers/search.js
@@ -1,30 +1,27 @@
 const Alpaca = require("@alpacahq/alpaca-trade-api");
 
 const alpaca = new Alpaca({
-    keyId: process.env.API_KEY,
-    secretKey: process.env.API_SECRET,
-    paper: true,
-  });
+  keyId: process.env.API_KEY,
+  secretKey: process.env.API_SECRET,
+  paper: true,
+});
 
+module.exports = {
+  getTicker: async (req, res) => {
+    try {
+      let trade;
 
-  module.exports = {
-      getTicker: async (req,res)=>{
-
-          console.log(req.params.id)
-         try{
-            let trade
-        
-            if(req.body.type === 'stock'){
-            trade = await alpaca.getLatestTrade(req.params.id);
-            
-            }else{
-            trade = await alpaca.getLatestCryptoTrade(`${req.params.id}USD`,{exchange:'FTXU'})
-            }
-            console.log(trade)
-            res.json({stock:trade,type:req.body.type})
-         }catch(err){
-            console.log(err)
-            res.json('error')
-         }
+      if (req.body.type === "stock") {
+        trade = await alpaca.getLatestTrade(req.params.id);
+      } else {
+        trade = await alpaca.getLatestCryptoTrade(`${req.params.id}USD`, {
+          exchange: "FTXU",
+        });
       }
-  } 
+      res.json({ stock: trade, type: req.body.type });
+    } catch (err) {
+      console.log(err);
+      res.json("error");
+    }
+  },
+};

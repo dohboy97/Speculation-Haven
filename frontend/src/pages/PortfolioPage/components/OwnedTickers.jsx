0ba47483f10db76ce17bc20a4518472fd324f397
Connor Doherty <92645318+dohboy97@users.noreply.github.com>
2023-02-25 00:46:49 -0500

    added loader for portfolio page

--- frontend/src/pages/PortfolioPage/components/OwnedTickers.jsx
+++ frontend/src/pages/PortfolioPage/components/OwnedTickers.jsx
@@ -1,15 +1,28 @@
-import { Typography, Button, Grid } from "@mui/material";
+import { Typography, Box, Button, Grid, Skeleton } from "@mui/material";
 import { useEffect, useState } from "react";
 import { getPortfolio } from "../../../api";
 
 export default function OwnedTickers() {
   const [ownedTickers, setOwnedTickers] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    getPortfolio().then((portfolio) =>
-      setOwnedTickers(portfolio.portfolio[0].ownedTickers)
-    );
+    setIsLoading(true);
+    getPortfolio()
+      .then((portfolio) => setOwnedTickers(portfolio.portfolio[0].ownedTickers))
+      .finally(() => setIsLoading(false));
   }, []);
+
+  if (isLoading) {
+    return (
+      <Box width="50%">
+        <Skeleton height={32} />
+        <Skeleton height={32} />
+        <Skeleton height={32} />
+        <Skeleton height={32} />
+      </Box>
+    );
+  }
   if (!ownedTickers) return null;
   return (
     <Grid width="75%" container>

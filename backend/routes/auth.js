b7c77bcd3ef5b703d3dc003144342d5b2c354571
Connor Doherty <92645318+dohboy97@users.noreply.github.com>
2023-04-09 10:21:02 -0400

    creating login status ping

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,8 +8,24 @@ router.get(
   passport.authenticate("google", { failureRedirect: "/login" }),
   function (req, res) {
     // Successful authentication, redirect home.
+
     res.redirect("http://localhost:3001/watchlist");
   }
 );
+router.get("/status", (req, res) => {
+  if (req.user) {
+    res.status(200).json({
+      success: true,
+      message: "successful",
+      user: req.user,
+      //cookies: req.cookies
+    });
+  } else {
+    res.status(401).json({
+      success: false,
+      message: "failure",
+    });
+  }
+});
 
 module.exports = router;
--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -5,9 +5,14 @@ export default function LoginPage() {
     window.open("http://localhost:3000/auth/google");
   };
 
+  const ping = () => {
+    fetch("http://localhost:3000/auth/status").then((res) => console.log(res));
+  };
+
   return (
     <Box>
       <Button onClick={login}>Login</Button>
+      <Button onClick={ping}>Ping</Button>
     </Box>
   );
 }

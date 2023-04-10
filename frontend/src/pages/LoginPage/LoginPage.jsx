6c25e8c44f80b0a274c823c77746d803f6410228
Connor Doherty <92645318+dohboy97@users.noreply.github.com>
2023-04-10 16:45:32 -0400

    removing ping button

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -5,16 +5,9 @@ export default function LoginPage() {
     window.open("http://localhost:3000/auth/google");
   };
 
-  const ping = async () => {
-    const res = await fetch("/auth/status");
-    const userData = await res.json();
-    console.log(userData);
-  };
-
   return (
     <Box>
       <Button onClick={login}>Login</Button>
-      <Button onClick={ping}>Ping</Button>
     </Box>
   );
 }

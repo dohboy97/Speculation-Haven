706e9d41463770c95f13ea4f715dce9d3e43f2e1
Connor Doherty <92645318+dohboy97@users.noreply.github.com>
2023-09-17 21:25:16 -0400

    changing redirect and hosting routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const router = express.Router();
-const passport = require("passport");
+const express = require("express")
+const router = express.Router()
+const passport = require("passport")
 
-router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }))
 router.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/login" }),
   function (req, res) {
     // Successful authentication, redirect home.
 
-    res.redirect("http://localhost:3001/watchlist");
+    res.redirect("http://localhost:5000/watchlist")
   }
-);
+)
 router.get("/status", (req, res) => {
   if (req.user) {
     res.status(200).json({
@@ -19,22 +19,22 @@ router.get("/status", (req, res) => {
       message: "successful",
       user: req.user,
       //cookies: req.cookies
-    });
+    })
   } else {
     res.status(401).json({
       success: false,
       message: "failure",
-    });
+    })
   }
-});
+})
 
 router.get("/logout", function (req, res, next) {
   req.logout(function (err) {
     if (err) {
-      return next(err);
+      return next(err)
     }
-    res.redirect("http://localhost:3001/");
-  });
-});
+    res.redirect("http://localhost:5000/")
+  })
+})
 
-module.exports = router;
+module.exports = router

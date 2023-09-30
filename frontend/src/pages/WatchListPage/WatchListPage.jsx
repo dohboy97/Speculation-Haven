8e272abcebd69bcaa42c2908f990ed4885a38087
Connor Doherty <92645318+dohboy97@users.noreply.github.com>
2023-09-30 12:23:13 -0400

    adding empty state for watchlist page

--- frontend/src/pages/WatchListPage/WatchListPage.jsx
+++ frontend/src/pages/WatchListPage/WatchListPage.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect, useContext } from "react";
 import { Typography, Button, Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import Watchlist from "./components/Watchlist";
 import { getWatchList, updateWatchlistPrices } from "../../api";
 
 import { UserContext } from "../../context";
 
 function WatchListPage() {
+  const navigate = useNavigate();
   const user = useContext(UserContext);
   const userId = user._id;
   const [watchList, setWatchList] = useState([]);
@@ -32,25 +34,44 @@ function WatchListPage() {
       .finally(() => setIsLoading(false));
   }, [setWatchList, setIsLoading, userId]);
 
+  const hasWatchList = watchList.length > 0 && !isLoading;
+
   return (
-    <Box className="App">
+    <Box width="100%" height="100%" className="App">
       <Typography marginY={2} variant="h4">
         Watchlist
       </Typography>
 
-      <Button
-        sx={{ marginBottom: 4 }}
-        variant="contained"
-        onClick={handleUpdate}
-      >
-        Update Prices
-      </Button>
-
-      <Watchlist
-        watchlist={watchList}
-        setWatchList={setWatchList}
-        isLoading={isLoading}
-      />
+      {hasWatchList && (
+        <Box>
+          <Button
+            sx={{ marginBottom: 4 }}
+            variant="contained"
+            onClick={handleUpdate}
+          >
+            Update Prices
+          </Button>
+          <Watchlist
+            watchlist={watchList}
+            setWatchList={setWatchList}
+            isLoading={isLoading}
+          />
+        </Box>
+      )}
+      {!hasWatchList && !isLoading && (
+        <Box
+          display="flex"
+          flexDirection="column"
+          gap={2}
+          alignItems=" center"
+          justifyContent="center"
+        >
+          <Typography variant="h6">Your watchlist is empty</Typography>
+          <Button variant="outlined" onClick={() => navigate("/search")}>
+            Find tickers to add
+          </Button>
+        </Box>
+      )}
     </Box>
   );
 }

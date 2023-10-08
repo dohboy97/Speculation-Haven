50c7efd3ae74fcbbd48977467901069e2c837bfd
Connor Doherty <92645318+dohboy97@users.noreply.github.com>
2023-10-07 21:44:45 -0400

    small styling changes to login page

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -7,9 +7,32 @@ export default function LoginPage() {
   };
 
   return (
-    <Box>
-      <Typography>Please login to continue</Typography>
-      <Button onClick={login}>Login</Button>
+    <Box
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      gap={4}
+    >
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Typography variant="h2">Speculation Station</Typography>
+        <Typography variant="h6">Learn to trade via paper trading!</Typography>
+      </Box>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Typography>Please login to continue</Typography>
+        <Button onClick={login}>Login</Button>
+      </Box>
     </Box>
   );
 }
